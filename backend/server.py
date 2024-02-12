4980891ea449092eaedae7a443042b73f4adf6e4
majikyouser <sahiagb@gmail.com>
2024-02-12 16:55:58 -0500

    first commit

---
+++ backend/server.py
@@ -0,0 +1,18 @@
+from flask import Flask 
+ 
+app = Flask(__name__)
+ 
+ 
+@app.route('/data')
+def get_time():
+ 
+    return {
+        'Name':"geek", 
+        "Age":"22",
+        "Date":x, 
+        "programming":"python"
+        }
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
